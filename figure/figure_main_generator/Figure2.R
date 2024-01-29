621edb7f5c308ab1a457f1ba938a903099405786
FloBeni <florian.benitiere@gmail.com>
2024-01-29 14:07:26 +0100

    update

--- figure/figure_main_generator/Figure2.R
+++ figure/figure_main_generator/Figure2.R
@@ -11,10 +11,10 @@ spearman_method_aa = cor.test( dt_graph$tRNASE_copies, dt_graph$obs_codon,method
 
 pA = ggplot(dt_graph,aes(x= obs_codon / sum(obs_codon)*100,y=tRNASE_copies,label=amino_acid)) +
   geom_smooth(formula = y ~ x, method="lm", size=1 , col=set_color[1],se=F,linetype='dashed') +
-  geom_point(pch=21,size=5,fill=set_color[2]) +
-  geom_text(nudge_x = .35,size=7,family="economica") + theme_bw() +  theme(
+  geom_point(pch=21,size=4,fill=set_color[2]) +
+  geom_text(nudge_x = .35,size=5,family="economica") + theme_bw() +  theme(
     axis.title.x = element_text(color="black", size=26,family="economica"),
-    axis.title.y = element_text(color="black", size=26, family="economica"),
+    axis.title.y = element_text(color="black", size=25, family="economica"),
     axis.text.y =  element_text(color="black", size=22, family="economica"),
     axis.text.x =  element_text(color="black", size=22, family="economica"),
     title =  element_text(color="black", size=18, family="economica"),
@@ -30,7 +30,7 @@ pA = ggplot(dt_graph,aes(x= obs_codon / sum(obs_codon)*100,y=tRNASE_copies,label
   ) 
 pA
 
-jpeg(paste(path_pannel,"p2A.jpg",sep=""), width = 4000/1, height = 2500/1,res=450/1)
+jpeg(paste(path_pannel,"p2A.jpg",sep=""), width = 4000/1, height = 4000/1,res=700/1)
 print(pA)
 dev.off()
 
@@ -49,10 +49,10 @@ pB = ggplot(dt_graph,aes(y=rho_aa_fpkm,fill=clade_group,x=clade_group))  +
   geom_text (label="p-value < 0.05", y=.4,x="Lepido Diptera",size=5,family="economica",col="red") + geom_boxplot(alpha=.1) + 
   geom_point(aes(fill=clade_group),size=3,pch=21,alpha=0.7) + theme_bw() + theme(
     axis.title.x = element_text(color="black",angle = 50, size=25,family="economica"),
-    axis.title.y = element_text(color="black", size=30, family="economica"),
-    axis.text.y =  element_text(color="black", size=22, family="economica"),
+    axis.title.y = element_text(color="black", size=22, family="economica"),
+    axis.text.y =  element_text(color="black", size=20, family="economica"),
     axis.text.x =  element_text(color="black",vjust=1,hjust=1, size=22,angle = 30, family="economica"),
-    title =  element_text(color="black", size=15, family="economica"),
+    title =  element_text(color="black", size=0, family="economica"),
     legend.text =  element_text(color="black", size=20, family="economica")
   ) + theme(legend.position='none') + scale_fill_manual(values=Clade_color) + ylab("Spearmann Rho")  + xlab("") + ylim(0,1)
 
@@ -71,24 +71,25 @@ imgB = load.image(paste(path_pannel,"p2B.jpg",sep="") )
 Caenorhabditis_elegans = readPNG(paste(path_require,"Caenorhabditis_elegans.png",sep=""))
 
 {
-  pdf(file= paste(path_figure,"Figure2.pdf",sep=""), width=4, height=5)
+  pdf(file= paste(path_figure,"Figure2.pdf",sep=""), width=11, height=4)
   
-  m=matrix(rep(NA,10*10), nrow=10)
+  m=matrix(rep(NA,10*100), nrow=10)
   for(i in 1:10){
-    m[,i]=c(rep(1,5),rep(2,5))
+    m[i,]=c(rep(1,35),rep(2,65))
   }
+  m
   layout(m)
   
-  par(mar=c(0, 1, 1, 1))
+  par(mar=c(0, 2, 2, 1))
   plot(imgA, axes=FALSE)
-  mtext("A", adj=-0.1, side=2,at=1, line=1, font=2, cex=1.4,las=2)
-  xcel=500
-  ycel=-140
-  rasterImage(Caenorhabditis_elegans,xleft=0+xcel, ybottom=350/1.5-ycel, xright=1000/1.4+xcel, ytop=0-ycel)
+  mtext("A", adj=0, side=2,at=-200, line=1, font=2, cex=2.2,las=2)
+  # xcel=540
+  # ycel=-140
+  # rasterImage(Caenorhabditis_elegans,xleft=0+xcel, ybottom=350/1.5-ycel, xright=1000/1.4+xcel, ytop=0-ycel)
   
-  par(mar=c(0, 1, 0, 0))
+  par(mar=c(0, 0, 0, 0))
   plot(imgB, axes=FALSE)
-  mtext("B",adj=-0.1, side=2,at=1, line=1, font=2, cex=1.4,las=2)
+  mtext("B",adj=-1, side=2,at=150, line=1, font=2, cex=2.2,las=2)
   dev.off()
 }
 
