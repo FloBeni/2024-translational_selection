0404ff76a9346bf673253ae24a6aea3f4b0a39e3
FloBeni <florian.benitiere@gmail.com>
2024-02-01 15:33:05 +0100

    update

--- figure/figure_main_generator/Figure6_v2.R
+++ figure/figure_main_generator/Figure6_v2.R
@@ -7,12 +7,16 @@ set_shape_class = c(CDS=21,intron=24)
 set_linetype_class = c(CDS="solid",intron="dashed")
 set_alpha_class = c(CDS=1,intron=0.5)
 
-data_10 = read.delim("data/data10_supp.tab")
 
-dt_graph = data_10[ data_10$group == "POCs" ,]
+###
+
+data_11 = read.delim("data/data11_supp.tab")
+
+dt_graph = data_11[ data_11$group == "POCs" ,]
 
 
 # Pannel B
+
 pB = ggplot(dt_graph,aes(x=fpkm)) + ggtitle("POCs")
 
 pB = pB +
@@ -39,17 +43,17 @@ pB = pB + theme_bw() + theme(
   scale_x_log10(limits = c(0.01,1000),
                 breaks=c(0.005,0.01,0.1,0.5,1,5,10,50,100,1000,10000,50000),
                 labels=c(0.005,0.01,0.1,0.5,1,5,10,50,100,1000,10000,50000)) +
-  ylab(paste("Substitution rate within CDSs")) + scale_fill_manual("",values=set_color_class) + 
+  ylab(paste("SNPs density within CDSs")) + scale_fill_manual("",values=set_color_class) + 
   scale_color_manual("",values=set_color_class) + scale_linetype_manual("",values=set_linetype_class) +
   scale_shape_manual("",values=set_shape_class) + scale_alpha_manual("",values=set_alpha_class) +
   guides(fill= guide_legend(override.aes = list(size=7,pch=21,byrow = TRUE),order = 2)
   )  + annotation_logticks(sides="b")+
-  guides(color = FALSE, size = FALSE , linetype=F, pch=F, alpha=F) + theme(legend.position='none') + ylim(0.005,0.04)+
+  guides(color = FALSE, size = FALSE , linetype=F, pch=F, alpha=F) + theme(legend.position='none') + ylim(0.01,0.05)+
   theme(legend.position = c(0.8, 0.15),
         legend.background = element_rect(fill="NA"),
         legend.spacing.x = unit(0.5, 'cm'),
         legend.spacing.y = unit(0.5, 'cm')
-        )
+  )
 pB
 
 jpeg(paste(path_pannel,"p6B.jpg",sep=""), width = 6000/1, height = 2800/1,res=600/1)
@@ -57,11 +61,10 @@ print(pB)
 dev.off()
 
 
-
 # Pannel C
 
 pC = ggplot(dt_graph,aes(x=fpkm)) + ggtitle("POCs")
-colnames(dt_graph)
+
 pC = pC +
   geom_line(aes(y=simulation_density_optimal_to_nonoptimal_intron,color="POC -> non-POC",linetype='intron',alpha='intron'),size=1) +
   geom_point(aes(y=density_optimal_to_nonoptimal_intron,fill="POC -> non-POC",pch='intron',alpha='intron'),size=3)+
@@ -86,25 +89,28 @@ pC = pC + theme_bw() + theme(
   scale_x_log10(limits = c(0.01,1000),
                 breaks=c(0.005,0.01,0.1,0.5,1,5,10,50,100,1000,10000,50000),
                 labels=c(0.005,0.01,0.1,0.5,1,5,10,50,100,1000,10000,50000)) +
-  ylab(paste("Substitution rate within introns")) + scale_fill_manual("",values=set_color_class) + 
+  ylab(paste("SNPs density within introns")) + scale_fill_manual("",values=set_color_class) + 
   scale_color_manual("",values=set_color_class) + scale_linetype_manual("",values=set_linetype_class) +
   scale_shape_manual("",values=set_shape_class) + scale_alpha_manual("",values=set_alpha_class) +
   guides(fill= guide_legend(override.aes = list(size=7,pch=21,byrow = TRUE),order = 2)
   ) +   theme(legend.spacing.y = unit(.2, 'cm'))  + theme(legend.position='top') + annotation_logticks(sides="b")+
-  guides(color = FALSE, size = FALSE , linetype=F, pch=F, alpha=F) + theme(legend.position='none') + ylim(0.005,0.04)
+  guides(color = FALSE, size = FALSE , linetype=F, pch=F, alpha=F) + theme(legend.position='none') + ylim(0.01,0.05)
 pC
 
 jpeg(paste(path_pannel,"p6C.jpg",sep=""), width = 6000/1, height = 3000/1,res=600/1)
 print(pC)
 dev.off()
 
-###
-data_11 = read.delim("data/data11_supp.tab")
 
-dt_graph = data_11[ data_11$group == "POCs" ,]
+###
+
+data_10 = read.delim("data/data10_supp.tab")
+
+dt_graph = data_10[ data_10$group == "POCs" ,]
 
 
 # Pannel E
+
 pE = ggplot(dt_graph,aes(x=fpkm)) + ggtitle("POCs")
 
 pE = pE +
@@ -131,23 +137,22 @@ pE = pE + theme_bw() + theme(
   scale_x_log10(limits = c(0.01,1000),
                 breaks=c(0.005,0.01,0.1,0.5,1,5,10,50,100,1000,10000,50000),
                 labels=c(0.005,0.01,0.1,0.5,1,5,10,50,100,1000,10000,50000)) +
-  ylab(paste("SNPs density within CDSs")) + scale_fill_manual("",values=set_color_class) + 
+  ylab(paste("Substitution rate within CDSs")) + scale_fill_manual("",values=set_color_class) + 
   scale_color_manual("",values=set_color_class) + scale_linetype_manual("",values=set_linetype_class) +
   scale_shape_manual("",values=set_shape_class) + scale_alpha_manual("",values=set_alpha_class) +
   guides(fill= guide_legend(override.aes = list(size=7,pch=21,byrow = TRUE),order = 2)
   )  + annotation_logticks(sides="b")+
-  guides(color = FALSE, size = FALSE , linetype=F, pch=F, alpha=F) + theme(legend.position='none') + ylim(0.01,0.05)+
+  guides(color = FALSE, size = FALSE , linetype=F, pch=F, alpha=F) + theme(legend.position='none') + ylim(0.005,0.04)+
   theme(legend.position = c(0.8, 0.15),
         legend.background = element_rect(fill="NA"),
         legend.spacing.x = unit(0.5, 'cm'),
         legend.spacing.y = unit(0.5, 'cm')
-  )
+        )
 pE
 
 jpeg(paste(path_pannel,"p6E.jpg",sep=""), width = 6000/1, height = 2800/1,res=600/1)
 print(pE)
 dev.off()
-
 
 
 # Pannel F
@@ -178,12 +183,12 @@ pF = pF + theme_bw() + theme(
   scale_x_log10(limits = c(0.01,1000),
                 breaks=c(0.005,0.01,0.1,0.5,1,5,10,50,100,1000,10000,50000),
                 labels=c(0.005,0.01,0.1,0.5,1,5,10,50,100,1000,10000,50000)) +
-  ylab(paste("SNPs density within introns")) + scale_fill_manual("",values=set_color_class) + 
+  ylab(paste("Substitution rate within introns")) + scale_fill_manual("",values=set_color_class) + 
   scale_color_manual("",values=set_color_class) + scale_linetype_manual("",values=set_linetype_class) +
   scale_shape_manual("",values=set_shape_class) + scale_alpha_manual("",values=set_alpha_class) +
   guides(fill= guide_legend(override.aes = list(size=7,pch=21,byrow = TRUE),order = 2)
   ) +   theme(legend.spacing.y = unit(.2, 'cm'))  + theme(legend.position='top') + annotation_logticks(sides="b")+
-  guides(color = FALSE, size = FALSE , linetype=F, pch=F, alpha=F) + theme(legend.position='none') + ylim(0.01,0.05)
+  guides(color = FALSE, size = FALSE , linetype=F, pch=F, alpha=F) + theme(legend.position='none') + ylim(0.005,0.04)
 pF
 
 jpeg(paste(path_pannel,"p6F.jpg",sep=""), width = 6000/1, height = 3000/1,res=600/1)
@@ -193,10 +198,10 @@ dev.off()
 
 # Figure 6
 
-imgA = load.image(paste(path_require,"substitutions.png",sep="") )
+imgA = load.image(paste(path_require,"polymorphism.png",sep="") )
 imgB = load.image(paste(path_pannel,"p6B.jpg",sep="") )
 imgC = load.image(paste(path_pannel,"p6C.jpg",sep="") )
-imgD = load.image(paste(path_require,"polymorphism.png",sep="") )
+imgD = load.image(paste(path_require,"substitutions.png",sep="") )
 imgE = load.image(paste(path_pannel,"p6E.jpg",sep="") )
 imgF = load.image(paste(path_pannel,"p6F.jpg",sep="") )
 fly<-readPNG(paste(path_require,"Drosophila_melanogaster.png",sep=""))
@@ -218,7 +223,7 @@ fly<-readPNG(paste(path_require,"Drosophila_melanogaster.png",sep=""))
   mtext("A",at=-100,adj=-1, side=2, line=1, font=2, cex=1.7,las=2)
   xaxis=570/1.1
   yaxis=0/1.1
-  rasterImage(fly,xleft=0+xaxis, ybottom=0+yaxis, xright=1100/12+xaxis, ytop=-900/12+yaxis)
+  rasterImage(fly,xleft=0+xaxis, ybottom=0+yaxis, xright=1100/13+xaxis, ytop=-900/13+yaxis)
   par(mar=c(0, 0, 1, 0))
   plot(imgB, axes=FALSE)
   mtext("B",at=100,adj=-0.7, side=2, line=1, font=2, cex=1.6,las=2)
@@ -232,7 +237,7 @@ fly<-readPNG(paste(path_require,"Drosophila_melanogaster.png",sep=""))
   mtext("D",at=-100,adj=-1, side=2, line=1, font=2, cex=1.7,las=2)
   xaxis=570/1.1
   yaxis=0/1.1
-  rasterImage(fly,xleft=0+xaxis, ybottom=0+yaxis, xright=1100/12+xaxis, ytop=-900/12+yaxis)
+  rasterImage(fly,xleft=0+xaxis, ybottom=0+yaxis, xright=1100/11+xaxis, ytop=-900/11+yaxis)
   par(mar=c(0, 0, 1, 0))
   plot(imgE, axes=FALSE)
   mtext("E",at=100,adj=-0.7, side=2, line=1, font=2, cex=1.6,las=2)
